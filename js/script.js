825cf0349f7c91e276c8a08ab299d0af81a72963
mahadi40011 <mahadi40011@gmail.com>
2025-09-08 11:06:15 +0600

    all nassesary alart code writing compleate(lesson 4/7 & null data)

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const loadLesson = () => {
     .then((json) => displayLesson(json.data));
 };
 
-// every level words load hocche 
+// every level words load hocche
 const loadLevelWords = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
@@ -19,6 +19,18 @@ const displayLevelWords = (words) => {
   const wordsContainer = document.getElementById("words-container");
   wordsContainer.innerHTML = "";
 
+  //if kono lesson e word na thake tahole eta kaj korbe
+  if (words.length === 0) {
+    wordsContainer.innerHTML = `
+    <div class="col-span-full text-center lg:py-10 py-6 lg:space-y-3 space-y-2">
+        <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="font-bangla lg:text-xl text-sm text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <p class="font-bangla font-medium lg:text-4xl text-2xl">নেক্সট Lesson এ যান</p>
+    </div>
+    `;
+    return;
+  }
+
   //get into every lesson
   words.forEach((word) => {
     //create Element & set inner HTML
@@ -27,9 +39,9 @@ const displayLevelWords = (words) => {
     <div
         class="bg-white rounded-xl shadow-sm text-center py-6 lg:py-10 px-3 lg:px-5 flex flex-col gap-2 lg:gap-6"
     >
-        <h2 class="font-bold text-xl lg:text-3xl">${word.word}</h2>
+        <h2 class="font-bold text-xl lg:text-3xl">${word.word ? word.word : "word পাওয়া যায়নি"}</h2>
         <p class="font-medium text-sm lg:text-xl">meaning / pronunciation</p>
-        <div class="font-bangla font-semibold text-xl lg:text-3xl">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="font-bangla font-semibold text-xl lg:text-3xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}"</div>
         <div class="flex justify-between items-center px-3 lg:px-5 mt-6 lg:mt-10">
         <button
             type="button"
@@ -67,7 +79,7 @@ const displayLesson = (lessons) => {
     `;
     //append this child into container
     lessonContainer.append(btnDiv);
-  };
+  }
 };
 
 // lessons load function calling
